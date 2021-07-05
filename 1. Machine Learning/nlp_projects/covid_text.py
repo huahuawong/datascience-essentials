5486f50d02a62fb8f5d3a1e5a3b8adc8edeeb410
Jun Hua Wong <junhuawong96@gmail.com>
2021-07-05 11:53:48 -0400

    Add files via upload

--- 1. Machine Learning/nlp_projects/covid_text.py
+++ 1. Machine Learning/nlp_projects/covid_text.py
@@ -23,6 +23,7 @@ import re
 import gensim
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
+import tensorflow as tf
 from tqdm import tqdm
 from keras.models import Sequential
 from keras.layers import Embedding,LSTM,Dense,SpatialDropout1D
@@ -41,6 +42,8 @@ from PIL import Image
 from nltk.tokenize import word_tokenize
 from nltk.util import ngrams
 import string
+from sklearn.metrics import classification_report,confusion_matrix,accuracy_score
+
 nltk.download('stopwords')
 
 stop = set(stopwords.words('english'))
@@ -88,7 +91,8 @@ missing_data = missing_data.rename(columns={"index": " column name"})
 
 print("Null Values in each column:\n", missing_data)
 
-# We can see that there are 20% missing values in the 'Location' column, missing data as white lines
+# We can see that there are 20% missing values in the 'Location' column, missing data as white lines, however, they will
+# not be removed as we are not interested in location right now, we are looking at text data specifically
 import missingno as msno
 msno.matrix(df,color=(0.3,0.36,0.44))
 plt.show()
@@ -133,7 +137,7 @@ class_df = df.groupby('sentiment').count()['OriginalTweet'].reset_index().sort_v
 class_df.style.background_gradient(cmap='winter')
 
 percent_class = class_df.OriginalTweet
-labels  class_df.sentiment
+labels = class_df.sentiment
 
 colors = ['#17C37B','#F92969','#FACA0C']
 
@@ -141,10 +145,120 @@ my_pie,_,_ = plt.pie(percent_class,radius = 1.2,labels=labels,colors=colors,auto
 plt.setp(my_pie, width=0.6, edgecolor='white')
 plt.show()
 
-# fig=make_subplots(1,2,subplot_titles=('Train set','Test set'))
-# x=train.sentiment.value_counts()
-# fig.add_trace(go.Bar(x=x.index,y=x.values,marker_color=['#17C37B','#F92969','#FACA0C'],name='train'),row=1,col=1)
-# x=test.sentiment.value_counts()
-# fig.add_trace(go.Bar(x=x.index,y=x.values,marker_color=['#17C37B','#F92969','#FACA0C'],name='test'),row=1,col=2)
-# plt.show()
+# Text cleaning
+# 1. Loading the stop words that will be filtered out from the text
+# 2. Clean the data by remving the urls, mentions, hashtags and digits from the text. This can be done using regex
 
+
+# load stop words
+stop_word = stopwords.words('english')
+
+
+def clean(text):
+    # remove urls
+    text = re.sub(r'http\S+', " ", text)
+    # remove mentions
+    text = re.sub(r'@\w+', ' ', text)
+    # remove hashtags
+    text = re.sub(r'#\w+', ' ', text)
+    # remove digits
+    text = re.sub(r'\d+', ' ', text)
+    # remove html tags
+    text = re.sub('r<.*?>', ' ', text)
+    # remove stop words
+    text = text.split()
+    text = " ".join([word for word in text if not word in stop_word])
+    return text
+
+
+train['OriginalTweet'] = train['OriginalTweet'].apply(lambda x: clean(x))
+test['OriginalTweet'] = test['OriginalTweet'].apply(lambda x: clean(x))
+train.head()
+
+# Notice how the first row's OriginalTweet is empty now? That's because the entire tweet was filled with mentions, and
+# stopwords, so the empty rows will now be removed from the dataset
+train_df = train[train['OriginalTweet'] != '']
+test_df = test[test['OriginalTweet'] != '']
+
+train.shape
+train_df.shape
+# 15 rows were empty on the ['OriginalTweet'] column
+
+df_train = train_df[['OriginalTweet', 'sentiment']]; df_test = test_df[['OriginalTweet', 'sentiment']]
+
+x_train = df_train['OriginalTweet']; y_train = df_train['sentiment']
+x_test = df_test['OriginalTweet']; y_test = df_test['sentiment']
+
+max_len = np.max(x_train.apply(lambda x :len(x)))
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(x_train)
+vocab_length = len(tokenizer.word_index) + 1
+
+x_train = tokenizer.texts_to_sequences(x_train)
+x_test = tokenizer.texts_to_sequences(x_test)
+
+x_train = pad_sequences(x_train, maxlen=max_len, padding='post')
+x_test = pad_sequences(x_test, maxlen=max_len, padding='post')
+
+print(f"Vocab length: {vocab_length} and the max sequence length: {max_len}")
+embedding_dim = 16
+
+# Data modeling
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_length, embedding_dim, input_length=max_len),
+    tf.keras.layers.Bidirectional(tf.keras.layers.GRU(256, return_sequences=True)),
+    tf.keras.layers.GlobalAveragePooling1D(),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dropout(0.4),
+    tf.keras.layers.Dense(1, activation='softmax')
+])
+
+# opt = tf.keras.optimizers.Adam(learning_rate=0.01)
+model.compile(loss='categorical_crossentropy',optimizer="adam",metrics=['accuracy'])
+model.summary()
+
+x_train.shape, y_train.shape, x_test.shape, y_test.shape
+
+# encode the sentiment to integer values
+from keras.utils import to_categorical
+
+clean_up = {"sentiment": {"negative": 0, "neutral": 1, "positive": 2}}
+y_train = pd.DataFrame(y_train); y_test = pd.DataFrame(y_test)
+
+y_train = y_train.replace(clean_up)
+y_test = y_test.replace(clean_up)
+
+num_epochs = 10
+history = model.fit(x_train, y_train, epochs=num_epochs,
+                    validation_data=(x_test, y_test))
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+print(f"Accuracy on training data is:- {acc[-1]*100} %")
+print(f"Loss {loss[-1]*100}")
+
+print(f"Accuracy on validation data is:- {val_acc[-1]*100} %")
+print(f"Loss {val_loss[-1]*100}")
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc,'b',label='training acc')
+plt.plot(epochs, val_acc, 'r', label='validation acc')
+plt.legend()
+plt.show()
+
+
+plt.plot(epochs, loss,'b',label='training loss')
+plt.plot(epochs, val_loss, 'r', label='validation loss')
+plt.legend()
+plt.show()
+
+pred = model.predict_classes(x_test)
+
+# Confusion matrix
+cm = confusion_matrix(np.argmax(y_test,1),pred)
+cm
