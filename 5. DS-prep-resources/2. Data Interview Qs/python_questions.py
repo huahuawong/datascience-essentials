9262bffad52772d27840f655ede80662f11f0833
Jun Hua Wong <junhuawong96@gmail.com>
2021-08-03 23:50:53 -0400

    Create python_questions.py

---
+++ 5. DS-prep-resources/2. Data Interview Qs/python_questions.py
@@ -0,0 +1,5 @@
+# This problem is part of your free preview of Interview Query Premium.
+# This question was asked by: Splunk
+
+# We're given a string of integers that represent page numbers.
+# Write a function to return the last page number in the string. If the string of integers is not in correct page order, return the last number in order.
